b6d3b059cab31d7e996344b8bb49f2125fc40a93
Naitik0909 <parmarnaitik0909@gmail.com>
2020-08-06 22:00:46 +0530

    Directory structure fixed

--- totale/totale/urls.py
+++ totale/totale/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from commerce_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.HomePage.as_view(), name='index')
+    path('',views.Homepage.as_view(), name='index')
 ]
